f880d1313c3eb780f1055e08daea1cd5f6ac9ec4
mateus.borja <mateus.borja@gmail.com>
2020-11-06 17:03:23 -0400

    java if and elfe

---
+++ src/com/mateusborja/javaifelse/Frutaria.java
@@ -0,0 +1,49 @@
+package com.mateusborja.javaifelse;
+
+import java.util.Scanner;
+
+public class Frutaria {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("qtd morango em Kg: ");
+        double qtdMorango = sc.nextDouble();
+
+        System.out.print("qtd maça em Kg: ");
+        double qtdMaca = sc.nextDouble();
+
+        double precoKgMorango = 0.0;
+        double precoKgMaca = 0.0;
+
+        if (qtdMorango <= 5) {
+            precoKgMorango = 12.0; // vlr do kg;
+
+        } else {
+            precoKgMorango = 10.0; // vlr do kg;
+        }
+
+        if (qtdMaca <= 5) {
+            precoKgMaca = 8.0;
+        } else {
+            precoKgMaca = 5.0;
+
+        }
+
+        double prectoTotalMorango = qtdMorango * precoKgMorango;
+        double precoTotalMaca = qtdMaca * precoKgMaca;
+
+        double precoParcial = precoKgMorango + precoTotalMaca;
+        double precoTotal = precoParcial;
+
+        if ((qtdMorango + qtdMaca) > 8 || precoParcial > 25) {
+            precoTotal = precoParcial - (precoParcial / 100) * 10.0;
+        }
+
+        System.out.printf("%nTotal a pagar: R$ %.2f%n", precoTotal);
+
+        sc.close();
+
+
+    }
+}
