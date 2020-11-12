17ca27413d7b747a127c5727430369fb04ffa6f6
mateus.borja <mateus.borja@gmail.com>
2020-11-11 23:40:47 -0400

    collections, iteration, lists

--- src/com/mateusborja/javamethods/ex01/MainPerson.java
+++ src/com/mateusborja/javamethods/ex01/MainPerson.java
@@ -12,70 +12,77 @@ public class MainPerson {
 
         Person student1 = new Person();
         Student idstudent1 = new Student();
-        Address addressstudent1 = new Address();
 
         Person student2 = new Person();
         Student idstudent2 = new Student();
-        Address addressstudent2 = new Address();
 
 
-        //student1 main attributes through object student1
+        //student1
         idstudent1.setIdNumber(293);
         student1.setName("Philip Baldruing");
         student1.setPhone(558080000);
         student1.setEmail("philip@bolgrind.com");
-        persons.add(student1); //arraylist address;
+        persons.add(student1); //arraylist addressList;
 
-        //student2 main attributes through object student2
+        Address addressList = new Address();
+
+        //student2
         idstudent2.setIdNumber(103);
         student2.setName("Joaquim Baldruing");
         student2.setPhone(338080000);
         student2.setEmail("joaquim@bolgrind.com");
         persons.add(student2);
 
-        //student1 address1 attributes through object addressstudent1
-        addressstudent1.setStreet("R.Fourlad Street, N.09");
-        addressstudent1.setCity("Salt Lake City");
-        addressstudent1.setState("UTA");
-        addressstudent1.setPostalCode(1003022);
-        addressstudent1.setCountry("USA");
-        address.add(addressstudent1); //arraylist address;
+        addressList = new Address();
 
-        //student1 address2 attributes through object addressstudent1
-        addressstudent1.setStreet("Avenue Maryland Street, N.1509");
-        addressstudent1.setCity("New York");
-        addressstudent1.setState("NY");
-        addressstudent1.setPostalCode(101022);
-        addressstudent1.setCountry("USA");
-        address.add(addressstudent1); //arraylist address;
+        //student1 addressList attributes through object addressList
+        addressList.setStreet("R.Fourlad Street, N.09");
+        addressList.setCity("Salt Lake City");
+        addressList.setState("UTA");
+        addressList.setPostalCode(1003022);
+        addressList.setCountry("USA");
 
-        //student2 address1 attributes  through object addressstudent2
-        addressstudent2.setStreet("R.Fivelouard Street, N.19");
-        addressstudent2.setCity("Salt Lake City");
-        addressstudent2.setState("UTA");
-        addressstudent2.setPostalCode(1003022);
-        addressstudent2.setCountry("USA");
-        address.add(addressstudent2); //arraylist address;
+        address.add(addressList); //arraylist addressList;
+
+        addressList = new Address();
+
+        //student1 address2
+        addressList.setStreet("Avenue Maryland Street, N.1509");
+        addressList.setCity("New York");
+        addressList.setState("NY");
+        addressList.setPostalCode(101022);
+        addressList.setCountry("USA");
+        address.add(addressList); //arraylist addressList;
+
+
+        //student2 addressList
+        addressList = new Address();
+        addressList.setStreet("R.Fivelouard Street, N.19");
+        addressList.setCity("Salt Lake City");
+        addressList.setState("UTA");
+        addressList.setPostalCode(1003022);
+        addressList.setCountry("USA");
+        address.add(addressList); //arraylist addressList;
 
         System.out.println();
         System.out.println("Person 1");
         System.out.println(idstudent1.toString());
         System.out.println(student1.toString());
-        System.out.println(addressstudent1.toString());
+        System.out.println(addressList.toString());
         System.out.println();
 
         System.out.println("Person 2");
         System.out.println(idstudent2.toString());
         System.out.println(student2.toString());
-        System.out.println(addressstudent2.toString());
+        System.out.println(addressList.toString());
         System.out.println();
 
         //iteracao e impressao
         for (int i = 0; i < address.size(); i++) {
-            System.out.println(address.get(i));
-        }
+           System.out.println(address.get(i));
+       }
 
-        //System.out.println(address);
+
 
 
     }
