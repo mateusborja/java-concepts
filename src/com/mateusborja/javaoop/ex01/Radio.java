217b8db819a6f11fa92d56b0764de7022bbdb37a
mateus.borja <mateus.borja@gmail.com>
2020-11-13 00:52:20 -0400

    java oop com encapsulamento, heranca, cl abstratas

---
+++ src/com/mateusborja/javaoop/ex01/Radio.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * Mateus Borja // UX Designer • 3D Artist • Developer
+ * www.mateusborja.life
+ */
+
+package com.mateusborja.javaoop.ex01;
+
+public class Radio extends Eletrodomestico {
+
+
+    public static final short AM = 1;
+    public static final short FM = 2;
+    private int banda;
+    private float sintonia;
+    private int volume;
+
+
+    public Radio(int voltagem) {
+        super(voltagem, true);
+        setBanda(Radio.FM);
+        setSintonia(0);
+        setVolume(0);
+    }
+
+    public void desligar() {
+        super.setLigado(false);
+        setSintonia(0);
+        setVolume(0);
+    }
+
+    public void ligar() {
+        super.setLigado(true);
+        setSintonia(88.1f);
+        setVolume(25);
+    }
+
+    public int getBanda() {
+        return banda;
+    }
+
+
+    public void setBanda(int banda) {
+        this.banda = banda;
+    }
+
+    public float getSintonia() {
+        return sintonia;
+    }
+
+    public void setSintonia(float sintonia) {
+        this.sintonia = sintonia;
+    }
+
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    @Override
+    public String toString() {
+        return "Radio{" +
+                "banda=" + banda +
+                ", sintonia=" + sintonia +
+                ", volume=" + volume +
+                '}';
+    }
+}
