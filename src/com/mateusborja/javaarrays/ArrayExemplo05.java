91155253047b5103dcbf9f48a1114de6d01cbbbc
Mateus Borja <mateus.borja@gmail.com>
2020-11-16 22:13:00 -0400

    array ns. aleatorios 10 a 50

---
+++ src/com/mateusborja/javaarrays/ArrayExemplo05.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * Mateus Borja // UX Designer • 3D Artist • Developer
+ * www.mateusborja.life
+ */
+
+package com.mateusborja.javaarrays;
+
+import java.util.Random;
+
+public class ArrayExemplo05 {
+
+    public static void main(String[] args) {
+
+        Random r = new Random();
+
+        int[] array = new int[5];
+        int i;
+        int sum = 0;
+
+        //numeros aleatorios entre 10 <= 50
+        for (i = 0; i < array.length; i++) {
+            array[i] = 10 + r.nextInt(40);
+            System.out.print(" " + array[i]);
+            sum += array[i];
+        }
+
+        System.out.println();
+        System.out.println("Soma dos aleatorios = " + sum);
+
+
+    }
+
+
+}
