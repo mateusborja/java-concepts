b7bb5b5cd68929c2237d9e904f21a12ba652d29c
Mateus Borja <mateus.borja@gmail.com>
2020-11-16 22:17:10 -0400

    java while soma dos numeros digitados ate o digitar o zero

--- src/com/mateusborja/javawhile/JavaWhileExemplo02.java
+++ src/com/mateusborja/javawhile/JavaWhileExemplo02.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * Mateus Borja // UX Designer • 3D Artist • Developer
+ * www.mateusborja.life
+ */
+
+//soma dos numeros digitados ate zero
 package com.mateusborja.javawhile;
 
 import java.util.Scanner;
@@ -8,11 +15,12 @@ public class JavaWhileExemplo02 {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("digite numeros que deseja somar, no final digite zero:  ");
         int x = sc.nextInt();
 
         int soma = 0;
         while (x !=0) {
-            soma = soma + x; // soma += x;
+            soma += x; //soma = soma + x;
             x = sc.nextInt();
         }
         System.out.printf("A soma dos numeros é: %d%n",soma);
