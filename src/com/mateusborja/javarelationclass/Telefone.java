59eb7123e2dd7036dc3a357eb07885301061237b
Mateus Borja <mateus.borja@gmail.com>
2020-11-19 17:12:12 -0400

    classe Telefone def

---
+++ src/com/mateusborja/javarelationclass/Telefone.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * Mateus Borja // UX Designer • 3D Artist • Developer
+ * www.mateusborja.life
+ */
+
+package com.mateusborja.javarelationclass;
+
+public class Telefone {
+
+    private String tipo;
+    private String ddd;
+    private String numero;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString() + "Tipo: " + this.tipo + ", DDD:  " + this.ddd + ", " + "Numero: " + this.numero;
+    }
+
+
+}
