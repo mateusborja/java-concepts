2c7cfc7417f4a8991c4ec8741e32c3f399c61d42
Mateus Borja <mateus.borja@gmail.com>
2020-11-19 17:11:01 -0400

    classe Endereco

---
+++ src/com/mateusborja/javarelationclass/Endereco.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * Mateus Borja // UX Designer • 3D Artist • Developer
+ * www.mateusborja.life
+ */
+
+package com.mateusborja.javarelationclass;
+
+public class Endereco {
+
+    private String rua;
+    private String numero;
+    private String complemento;
+    private String cep;
+    private String cidade;
+    private String estado;
+
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Rua: " + this.rua + ", N. " + this.numero + ", " + "Complemento: " + this.complemento + ", " + "Cep: " + this.cep + ", " + "Cidade: " +
+                this.cidade + ", " + "Estado: " + this.estado;
+    }
+}
