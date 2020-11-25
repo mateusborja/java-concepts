c1353f2a77c3181fdacb0bba1b947ca94acd9697
Mateus Borja <mateus.borja@gmail.com>
2020-11-25 01:29:18 -0400

    busca em array

---
+++ src/com/mateusborja/javaarrays/BuscaEmArray.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * Mateus Borja // UX Designer • 3D Artist • Developer
+ * www.mateusborja.life
+ */
+
+package com.mateusborja.javaarrays;
+
+import java.util.Scanner;
+
+public class BuscaEmArray {     
+
+    public static boolean buscaEmArray(int x, int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+
+        int x = 0;
+        while(x < 10) {
+            System.out.println("Digite o " + (x + 1) + "o número: ");
+            arr[x] = sc.nextInt();
+            x++;
+        }
+
+        System.out.println("Buscar por: " );
+        int busca = sc.nextInt();
+        boolean resultado = buscaEmArray(busca, arr);
+        System.out.println(resultado);
+
+        sc.close();
+
+
+    }
+
+}
