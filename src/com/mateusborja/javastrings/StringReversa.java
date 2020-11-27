e2d286f0bfea2fabf87b0135924a79f4348e5998
Mateus Borja <mateus.borja@gmail.com>
2020-11-26 22:09:23 -0400

    String reversa

--- src/com/mateusborja/javastrings/StringReversa.java
+++ src/com/mateusborja/javastrings/StringReversa.java
@@ -10,19 +10,15 @@ public class StringReversa {
 
     public static void main(String[] args) {
 
-
         String name = "The skye is blue";
-        char [] arr = name.toCharArray();
-
+        char[] arr = name.toCharArray();
 
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
-
         }
 
         System.out.println();
-
-        for (int j = arr.length -1; j >=0 ; j--) {
+        for (int j = arr.length - 1; j >= 0; j--) {
             System.out.print(arr[j]);
 
         }
