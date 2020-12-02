3f8a22e2b1e3b0659951e7a80ad61b8ede69338d
Mateus Borja <mateus.borja@gmail.com>
2020-12-02 11:11:49 -0400

    Array, metodo para adicionar, e ler utilizando as classes Arrays e StringBuilder

---
+++ src/com/mateusborja/javavetor/ex01/Vetor.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * Mateus Borja // UX Designer • 3D Artist • Developer
+ * www.mateusborja.life
+ */
+
+package com.mateusborja.javavetor.ex01;
+
+import java.util.Arrays;
+
+public class Vetor {
+
+    private String elementos[];
+    private int tamanho;
+
+    public Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+        this.tamanho = 0;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public int tamanhoDoVetor() {
+        return tamanho;
+    }
+
+    public boolean adicionarVetor(String elemento) {
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            tamanho++;
+            return true;
+        }
+
+        return false;
+    }
+
+    public String imprimirVetores() {
+        return Arrays.toString(elementos);
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+
+        for (int i = 0; i < this.tamanho -1; i++) {
+            s.append(this.elementos[i]);
+            s.append(", ");
+        }
+
+
+        return s.toString();
+    }
+
+
+}
