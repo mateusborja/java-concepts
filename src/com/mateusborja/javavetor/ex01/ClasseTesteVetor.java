8c624793e1a929d170acdaf77a2d573a7d99fade
Mateus Borja <mateus.borja@gmail.com>
2020-12-03 12:17:37 -0400

    classe de teste do vetor

--- src/com/mateusborja/javavetor/ex01/ClasseTesteVetor.java
+++ src/com/mateusborja/javavetor/ex01/ClasseTesteVetor.java
@@ -12,8 +12,8 @@ public class ClasseTesteVetor {
 
         Vetor arr = new Vetor(8);
         arr.adicionarVetor("desenvolvimento java");
-        arr.adicionarVetor("instituto");
-        arr.adicionarVetor("itn");
+        arr.adicionarVetor("teste1");
+        arr.adicionarVetor("teste2");
         arr.adicionarVetor("mateus");
 
         System.out.println(arr);
@@ -22,7 +22,11 @@ public class ClasseTesteVetor {
 
         System.out.println(arr.buscarVetor(3));
 
-        System.out.print(arr.buscarVetorExistente("itn"));
+        System.out.println(arr.buscarVetorExistente("mateus"));
+
+        System.out.println(arr.buscarVetorExistenteOp2("teste1")); // retorna um booleano
+
+        System.out.println(arr.buscarVetorExistenteOp3("teste1")); // retorna o indice
 
 
 
