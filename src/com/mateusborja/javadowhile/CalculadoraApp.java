2583f1b9de49824bec4e15addaa49d3935f2e04c
Mateus Borja <mateus.borja@gmail.com>
2020-12-06 18:52:09 -0400

    calculadora com while

--- src/com/mateusborja/javadowhile/CalculadoraApp.java
+++ src/com/mateusborja/javadowhile/CalculadoraApp.java
@@ -18,13 +18,13 @@ public class CalculadoraApp {
 
         while (op != 5) {
 
-            System.out.print("num1: ");
+            System.out.print("\nnum1: ");
             int num1 = sc.nextInt();
 
             System.out.print("num2: ");
             int num2 = sc.nextInt();
 
-            System.out.println("OPERAÇÃO:");
+            System.out.println("\nOPERAÇÃO:");
             System.out.println("1 > somar");
             System.out.println("2 > subtrair");
             System.out.println("3 > multiplicar");
@@ -32,18 +32,25 @@ public class CalculadoraApp {
             System.out.println("5 > sair");
             op = sc.nextInt();
 
-            if (op == 1 && op == 2 && op == 3 && op == 4 && op == 5) {
-                if (op == 1) {
-                    somar(num1, num2);
-                } else if (op == 2) {
-                    subtrair(num1, num2);
-                } else if (op == 3) {
-                    multiplicar(num1, num2);
-                } else if (op == 4) {
-                    dividir(num1, num2);
-                } else if (op == 5) {
-                    System.exit(0);
-                }
+            if (op == 1) {
+                System.out.println("Resultado da Soma: " + somar(num1, num2));
+
+            } else if (op == 2) {
+                System.out.println("Resultado da Subtração: " + subtrair(num1, num2));
+
+            } else if (op == 3) {
+                System.out.println("Resultado da Multiplicação: " + multiplicar(num1, num2));
+
+            } else if (op == 4) {
+                System.out.println("Resultado da Divisão: " + dividir(num1, num2));
+
+            } else if (op == 5) {
+                System.out.println("Saindo...");
+                System.exit(0);
+
+            } else {
+                System.out.println("Opção Inválida");
+                System.exit(0);
             }
 
         }
