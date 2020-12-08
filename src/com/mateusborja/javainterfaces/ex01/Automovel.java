7608ceff8f188e6af2213206cbf7aefe82d4c0c5
Mateus Borja <mateus.borja@gmail.com>
2020-12-08 11:54:40 -0400

    classe automovel - heranca

--- src/com/mateusborja/javainterfaces/ex01/Automovel.java
+++ src/com/mateusborja/javainterfaces/ex01/Automovel.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * Mateus Borja // UX Designer • 3D Artist • Developer
+ * www.mateusborja.life
+ */
+
 package com.mateusborja.javainterfaces.ex01;
 
 public interface Automovel {
