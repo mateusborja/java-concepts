08204ee5558ba8e7847bddbeb51f439f999487b7
Mateus Borja <mateus.borja@gmail.com>
2020-12-11 14:50:13 -0400

    classe teste DiaSemana

---
+++ src/com/mateusborja/javaenum/ex03/AplicacaoDiaSemana.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * Mateus Borja // UX Designer • 3D Artist • Developer
+ * www.mateusborja.life
+ */
+
+package com.mateusborja.javaenum.ex03;
+
+public class AplicacaoDiaSemana {
+
+    public static void main(String[] args) {
+
+        DiaSemana dia = DiaSemana.SEGUNDA;
+        System.out.println(dia.toString() + " - " + dia.getValor());
+    }
+}
