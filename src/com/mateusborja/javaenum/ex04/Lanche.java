5fe7bbc4d99be26290a2e7b4768834edcc36ab53
Mateus Borja <mateus.borja@gmail.com>
2020-12-12 10:29:55 -0400

    exemplo enum lance

---
+++ src/com/mateusborja/javaenum/ex04/Lanche.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * Mateus Borja // UX Designer • 3D Artist • Developer
+ * www.mateusborja.life
+ */
+
+package com.mateusborja.javaenum.ex04;
+
+public class Lanche {
+
+    public enum Bebida {
+        COCA_COLA("Coca-Cola", 5),
+        FANTA("Fanta", 4.5f),
+        SPRITE("Sprite", 4f),
+        AGUA("Agua", 3f);
+
+
+        private String nome;
+        private float preco;
+
+        Bebida(String nome, float preco) {
+            this.nome = nome;
+            this.preco = preco;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public float getPreco() {
+            return preco;
+        }
+
+    }
+
+}
