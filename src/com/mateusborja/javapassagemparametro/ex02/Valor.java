02ce3824080cdf5a6f6438790ce57fb7e3eed2da
Mateus Borja <mateus.borja@gmail.com>
2020-12-15 16:16:40 -0400

    .class

---
+++ src/com/mateusborja/javapassagemparametro/ex02/Valor.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * Mateus Borja // UX Designer • 3D Artist • Developer
+ * www.mateusborja.life
+ */
+
+package com.mateusborja.javapassagemparametro.ex02;
+
+public class Valor {
+
+    int v;
+}
