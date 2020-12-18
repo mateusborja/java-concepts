f8ebbc23957288240d8496e5422c0d05112ca8d4
Mateus Borja <mateus.borja@gmail.com>
2020-12-18 19:02:25 -0400

    varargs

--- src/com/mateusborja/javavarargs/Varargs.java
+++ src/com/mateusborja/javavarargs/Varargs.java
@@ -6,16 +6,44 @@
 
 package com.mateusborja.javavarargs;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Varargs {
 
     public static void main(String[] args) {
 
         System.out.println("sum prt: " + sum(1, 4));
 
-        int[] arr = {1, 4, 6, 10};
-        System.out.println("sum arr: " + sum(arr));
+        int[] arr1 = {1, 4, 6, 10};
+        System.out.println("sum arr: " + sum(arr1));
 
         System.out.println("sum varargs: " + sum(32, 4, 3, 23, 43, 5));
+
+        int arr2[] = {23, 43, 4, 54, 3, 2, 1};
+
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(" " + arr2[i]);
+
+        }
+
+        List mylist = Arrays.asList(arr2);
+
+        ArrayList<Integer> arrList = new ArrayList<Integer>();
+
+        arrList.add(124);
+        arrList.add(1232);
+        arrList.add(1);
+        arrList.add(122);
+        arrList.add(2);
+        arrList.add(13);
+
+        Collections.sort(arrList);
+        System.out.println("\n" + arrList);
+
 
 
 
